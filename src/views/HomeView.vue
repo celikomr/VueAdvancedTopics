820270ebee7645788a58c11cd17a831d1be6fe90
zeynep yıldız <zeynepyildiz113@gmail.com>
2022-10-07 12:15:03 +0300

    feat:added to todo-app components and tests

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TodoApp from '../components/todoComponents/TodoApp.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TodoApp/>
   </main>
 </template>
