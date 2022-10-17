7e2f891156f9721d030e708045ae5dccfc37cddd
Ömer Çelik <oclk@outlook.com.tr>
2022-10-17 18:53:33 +0300

    feat: Created protos folder

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+import { addTodoParams, getTodoParams, deleteTodoParams } from "todo_pb";
+import { todoServiceClient } from "todo_grpc_web_pb";
+
+</script>
+
 <template>
     <div>Users</div>
 </template>
